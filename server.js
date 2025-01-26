5c97648a87116180f764e5e169925974c56881f2
Om Patel <om28patel2004@gmail.com>
2025-01-26 18:11:34 +0530

    feat: socket initialise

---
+++ server.js
@@ -0,0 +1,35 @@
+const express = require("express");
+const http = require("http");
+const { Server } = require("socket.io");
+
+const app = express();
+const server = http.createServer(app);
+
+const io = new Server(server, {
+  cors: {
+    origin: "http://localhost:3000",
+    methods: ["GET", "POST"],
+  },
+});
+
+io.on("connection", (socket) => {
+  console.log("User connected:", socket.id);
+
+  socket.on("join_room", (room) => {
+    socket.join(room);
+    console.log(`User ${socket.id} joined room: ${room}`);
+  });
+
+  socket.on("send_message", (data) => {
+    console.log(data);
+    io.to(data.room).emit("receive_message", data.message);
+  });
+
+  socket.on("disconnect", () => {
+    console.log("User disconnected:", socket.id);
+  });
+});
+
+server.listen(3001, () => {
+  console.log("Server is running on http://localhost:3001");
+});
